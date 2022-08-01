0b4d90b40d1bc24d3b07e204a60dbfdfd5cf6761
Kerimov Ahmed <kerimov.dev@yandex.ru>
2022-08-01 20:38:03 +0300

    Create Subscription model

--- backend_subscription/backend_subscription/settings.py
+++ backend_subscription/backend_subscription/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'subscription_service.apps.SubscriptionServiceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- backend_subscription/subscription_service/models.py
+++ backend_subscription/subscription_service/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Subscription(models.Model):
+    subscription_uid = models.UUIDField(default=uuid.uuid4, editable=False)
+
+    class Type(models.TextChoices):
+        USER = 'user', _('user')
+        TAG = 'tag', _('tag')
+
+    type = models.CharField(max_length=4, choices=Type.choices)
+    follower_uid = models.UUIDField(default=uuid.uuid4, editable=False)
+    following_uid = models.UUIDField(default=uuid.uuid4, editable=False)
